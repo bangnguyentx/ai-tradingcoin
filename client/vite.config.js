6b4892ab1d32f37142fa33f15638b795bf07736b
bangnguyentx <nlbo010708@gmail.com>
2025-09-29 18:16:02 +0700

    vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// cấu hình build Vite
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist"
-  },
-  server: {
-    port: 3000
   }
 });
